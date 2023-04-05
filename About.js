069741aa9e33506850f3a7d478c87862c494bd00
Shruthika <100517924+Shruthika16@users.noreply.github.com>
2023-04-05 14:33:34 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h2>Your search for travel is over ! </h2>
+    </div>
+  )
+}
+
+export default About
---
+++ Contact.js
@@ -0,0 +1,8 @@
+
+const Contact = () => {
+    return <h4>Contact page
+        Phone no:0468-8512 526
+    </h4>;
+  };
+  
+  export default Contact;
